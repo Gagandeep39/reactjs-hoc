362c07848427fe62ab5e8c6b0bdb091c9d06efbd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-24 22:12:44 +0530

    Created Second type of Higher order component

---
+++ src/hoc/withClassTwo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Another way of Creating Higher order componenet
+// Here we create a funtioon that eturns a functional component
+// Here wrapped Component is a reference to other component
+// className is sued for providing 
+// To use this, we also need Aux
+const withClassTwo = (WrappedComponent, className) => {
+  return props => (
+    <div className={className}>
+      <WrappedComponent />
+    </div>
+  );
+}
+
+export default withClassTwo;
