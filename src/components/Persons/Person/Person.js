623e7018f1582945e587f837996df9ddee75c493
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-24 15:06:51 +0530

    Converted Person COmponent to clas component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.module.css';
 
-const person = (props) => {
-  console.log('[Person.js] Rendering');
+class Person extends Component {
+  render() {
+    console.log('[Person.js] Rendering');
 
-  return (
-    <div className={classes.Person}>
-      <p> I'm a Person!</p>
-      <p>
-        Name provided as attribute: {props.name}, age: {props.age}{' '}
-      </p>
-      <p>Age {Math.floor(Math.random() * 100)}</p>
-      <p> {props.children} </p>
-      <button onClick={props.deleteButton}>Delete</button>
-      <br />
-      <input type='text' onChange={props.change} />
-    </div>
-  );
-};
+    return (
+      <div className={classes.Person}>
+        <p> I'm a Person!</p>
+        <p>
+          Name provided as attribute: {this.props.name}, age: {this.props.age}{' '}
+        </p>
+        <p>Age {Math.floor(Math.random() * 100)}</p>
+        <p> {this.props.children} </p>
+        <button onClick={this.props.deleteButton}>Delete</button>
+        <br />
+        <input type='text' onChange={this.props.change} />
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
