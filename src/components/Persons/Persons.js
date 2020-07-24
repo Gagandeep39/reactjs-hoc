fb87a4f9b14d5f8b9291b027d71e946933c872e3
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-24 15:05:30 +0530

    Comnverted Persons functional component into class

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  console.log('[Persons.js] Rendering');
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        key={person.id}
-        change={(event) => props.change(event, person.id)}
-        name={person.name}
-        age={person.age}
-        deleteButton={() => props.deleteButton(index)}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  render() {
+    console.log('[Persons.js] Rendering');
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          key={person.id}
+          change={(event) => this.props.change(event, person.id)}
+          name={person.name}
+          age={person.age}
+          deleteButton={() => this.props.deleteButton(index)}
+        />
+      );
+    });
+  }
+}
 
-export default persons;
+export default Persons;
